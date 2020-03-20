abef5c4fcf58d0fa653e8328ee1ef761e63bba77
fangtaozhu <zhufangtao@miduchina.com>
2020-03-20 18:31:25 +0800

    整合redis

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -8,10 +8,13 @@ import com.example.demo.pojo.User;
 import com.example.demo.service.UserService;
 import com.example.demo.untils.JwtUtil;
 import com.example.demo.untils.PasswordUtil;
+import io.jsonwebtoken.Claims;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 项目名称：springboot_demo
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
  * 类 描 述：UserController
  * 创建时间：2020/3/11 4:16 下午
  * 创 建 人：ZhuFangTao
+ *
  * @author fangtaozhu
  */
 @Slf4j
@@ -37,8 +41,8 @@ public class UserController {
     @GetMapping(value = "login")
     public Result login(@RequestParam String name, String password) throws Exception {
         Result result = new Result();
-        User user = userService.findUser(name,PasswordUtil.encrypt(password, Constant.ENCRYPTION_PASSWORD, PasswordUtil.getStaticSalt()));
-        if (user == null){
+        User user = userService.findUser(name, PasswordUtil.encrypt(password, Constant.ENCRYPTION_PASSWORD, PasswordUtil.getStaticSalt()));
+        if (user == null) {
             return result.setMsgCode(ResultCode.REQUEST_ERROR);
         }
         String token = new JwtUtil().createJWT(user.getId().toString(), JSON.toJSONString(user), Constant.EXP_TIME);
@@ -49,14 +53,26 @@ public class UserController {
 
     @ApiOperation(value = "用户注册")
     @PutMapping(value = "userRegister")
-    public Result userRegister(@RequestParam String name, String password){
+    public Result userRegister(@RequestParam String name, String password) {
         Result result = new Result();
         String encryptPassword = PasswordUtil.encrypt(password, Constant.ENCRYPTION_PASSWORD, PasswordUtil.getStaticSalt());
-        boolean blo = userService.addUser(name,encryptPassword);
-        if (blo){
+        boolean blo = userService.addUser(name, encryptPassword);
+        if (blo) {
             return result.setMsgCode(ResultCode.REQUEST_SUCCESS);
         }
         return result.setMsg("注册失败！");
     }
 
+    @ApiOperation(value = "验证token接口")
+    @PutMapping(value = "verifyToken")
+    public String verifyInterface(HttpServletRequest request) {
+        //验证token是否存在
+        try {
+            JwtUtil jwtUtil = new JwtUtil();
+            Claims claims = jwtUtil.parseJWT(request.getHeader("Authorization"));
+        } catch (Exception e) {
+            return "未登陆或长时间无操作登陆已过期！";
+        }
+        return "您现在的位置是页面A";
+    }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.untils.RedisUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+@Autowired
+private RedisUtil redisUtil;
 	@Test
 	void contextLoads() {
+		try {
+			redisUtil.set("xz","缓存测试");
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
 	}
 
 }
